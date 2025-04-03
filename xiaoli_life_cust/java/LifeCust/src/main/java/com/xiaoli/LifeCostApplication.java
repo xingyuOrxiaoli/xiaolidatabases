50aeae8edbcee0366e15f4f85d631876303f80ae
李星宇 <1723980130@qq.com>
2025-04-03 19:42:28 +0800

    项目目录构建并且配置前后端不分离的案例环境

---
+++ xiaoli_life_cust/java/LifeCust/src/main/java/com/xiaoli/LifeCostApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoli;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeCostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeCostApplication.class,args);
+    }
+}
